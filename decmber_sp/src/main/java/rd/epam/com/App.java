fe17119ca8af1c856dec66731f9b53229e9dc389
Michael <76106845+monsegard@users.noreply.github.com>
2020-12-22 14:09:28 +0300

    Update App.java

--- decmber_sp/src/main/java/rd/epam/com/App.java
+++ decmber_sp/src/main/java/rd/epam/com/App.java
@@ -10,9 +10,9 @@ public class App {
     public static void main(String[] args) {
         List<Student> students = new ArrayList();
 
-        Student student = new Student("Андрей", "Юрьевич");
+        Student student = new Student("Михаил", "Александрович");
         student.setGroup(Group.FIRST);
-        student.setSecondName("Иванов");
+        student.setSecondName("Руденок");
 
         students.add(student);
         students.forEach(System.out::println);
